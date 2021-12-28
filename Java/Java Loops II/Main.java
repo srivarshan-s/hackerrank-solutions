30843de8ee98693d0aa1e0ddf4d7b58a55c410c5
Srivarshan <srivarshan02@gmail.com>
2021-12-28 22:10:20 +0530

    Update Main.java

--- Java/Java Loops II/Main.java
+++ Java/Java Loops II/Main.java
@@ -5,6 +5,15 @@ class Main {
     public static void main(String []argh){
         Scanner scan = new Scanner(System.in);
         int q = scan.nextInt();
+        for (int i = 0; i < q; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int n = scan.nextInt();
+            for (int j = 0; j < n; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
         scan.close();
     }
 }
