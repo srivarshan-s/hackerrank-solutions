de49082f61bb2e5ba52eabdb84d0bad060eced1a
Srivarshan <srivarshan02@gmail.com>
2021-12-28 22:16:40 +0530

    Update Main.java

--- Java/Java Loops II/Main.java
+++ Java/Java Loops II/Main.java
@@ -1,8 +1,7 @@
 import java.util.*;
-import java.io.*;
 
-class Main {
-    public static void main(String []argh){
+public class Main {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int q = scan.nextInt();
         for (int i = 0; i < q; i++) {
@@ -10,10 +9,24 @@ class Main {
             int b = scan.nextInt();
             int n = scan.nextInt();
             for (int j = 0; j < n; j++) {
-                System.out.print(j + " ");
+                int sum = a;
+                for (int k = 0; k <= j; k++) {
+                    sum += pow(2, k) * b;
+                }
+                System.out.print(sum + " ");
             }
             System.out.println();
         }
         scan.close();
+    }
+
+    public static int pow(int num, int val) {
+        if (val == 1) {
+            return num;
+        } else if (val == 0) {
+            return 1;
+        } else {
+            return num * pow(num, val - 1);
+        }
     }
 }
