cfd36af35b803f35b6ad42773318e38a34992c99
Srivarshan <srivarshan02@gmail.com>
2021-12-28 21:50:03 +0530

    Update Main.java

--- Java/Java Loops II/Main.java
+++ Java/Java Loops II/Main.java
@@ -2,22 +2,9 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);    
-
+    public static void main(String []argh){
+        Scanner scan = new Scanner(System.in);
         int q = scan.nextInt();
-
-        for (int i = 0; i < q; ++i) {
-            int a = scan.nextInt();
-            int b = scan.nextInt();
-            int n = scan.nextInt();
-            for (int j = 0; j < n; ++j) {
-                int val = (a + 2*j) * b;
-                System.out.printf("%d ", val);
-            }
-            System.out.println();
-        }
-
         scan.close();
     }
 }
