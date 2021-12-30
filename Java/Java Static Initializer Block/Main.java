a4d605d166fc2eed657b3c0ff49596c2025962a8
Srivarshan <srivarshan02@gmail.com>
2021-12-30 10:41:54 +0530

    Update Main.java

--- Java/Java Static Initializer Block/Main.java
+++ Java/Java Static Initializer Block/Main.java
@@ -9,8 +9,20 @@ public class Main {
     // Write your code here
     public static Scanner scan = new Scanner(System.in);
     public static boolean flag = true;
-    public static int B = scan.nextInt();
-    public static int H = scan.nextInt();
+    public static int B;
+    public static int H;
+    static {
+        B = scan.nextInt();
+        H = scan.nextInt();
+        try {
+            if (B <= 0 || H <= 0) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println(e + ": Breadth and height must be positive");
+            flag = false;
+        }
+    }
 
     public static void main(String[] args) {
         if (flag) {
