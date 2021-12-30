a74c29776f5b63af8721b9994a37f1e3f4025bd9
Srivarshan <srivarshan02@gmail.com>
2021-12-30 09:28:35 +0530

    Create Java Static Initializer Block

---
+++ Java/Java Static Initializer Block/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+    // Write your code here
+    public static Scanner scan = new Scanner(System.in);
+    public static boolean flag = true;
+    public static int B = scan.nextInt();
+    public static int H = scan.nextInt();
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+
+    }// end of main
+
+}// end of class
