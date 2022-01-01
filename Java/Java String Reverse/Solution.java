ae90032df4627381433c177e67fd9c47b46ab4cb
Srivarshan <srivarshan02@gmail.com>
2022-01-01 20:17:39 +0530

    Update Solution.java

--- Java/Java String Reverse/Solution.java
+++ Java/Java String Reverse/Solution.java
@@ -1,13 +1,19 @@
-import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        
+
+        Scanner scanner= new Scanner(System.in);
+        String A = scanner.next();
+
+        String revOfA = new StringBuilder(A).reverse().toString();
+        if (A.compareTo(revOfA) == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        scanner.close();
     }
 }
