3b6ae331919f18a917e68ba8299cbeb1f1c1a359
Srivarshan <srivarshan02@gmail.com>
2022-01-01 19:56:48 +0530

    Update Solution.java

--- Java/Java Substring Comparisons/Solution.java
+++ Java/Java Substring Comparisons/Solution.java
@@ -3,16 +3,9 @@ import java.util.Scanner;
 public class Solution {
 
     public static String getSmallestAndLargest(String string, int k) {
-        String smallest = "";
-        String largest = "";
-
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
         int smallIdx = 0;
         int largeIdx = 0;
-        for (int i = 0; i < string.length() - k; i++) {
-            // System.out.println(s.substring(i, i+k));
+        for (int i = 0; i < string.length() - k + 1; i++) {
             String subString = string.substring(i, i + k);
             if (subString.compareTo(string.substring(smallIdx, smallIdx + k)) <= 0) {
                 smallIdx = i;
@@ -20,7 +13,6 @@ public class Solution {
             if (subString.compareTo(string.substring(largeIdx, largeIdx + k)) >= 0) {
                 largeIdx = i;
             }
-            System.out.println(subString);
         }
         return string.substring(smallIdx, smallIdx + k) + "\n" + string.substring(largeIdx, largeIdx + k);
     }
