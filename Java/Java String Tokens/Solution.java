c029c47c217a0fca04b33f54f09d12d25c3af31d
Srivarshan <srivarshan02@gmail.com>
2022-01-01 20:46:35 +0530

    Update Solution.java

--- Java/Java String Tokens/Solution.java
+++ Java/Java String Tokens/Solution.java
@@ -1,12 +1,29 @@
-import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        // Write your code here.
+        String string = scan.nextLine();
+
+        String[] tokens = string.split("[ !,?._'@]");
+
+        int numOfTokens = 0;
+        for (String token : tokens) {
+            if (token.compareTo(new String(" ")) == -1) {
+                continue;
+            }
+            numOfTokens++;
+        }
+        System.out.println(numOfTokens);
+
+        for (String token : tokens) {
+            if (token.compareTo(new String(" ")) == -1) {
+                continue;
+            }
+            System.out.println(token);
+        }
+
         scan.close();
     }
 }
