9152f54e39731d2dba466befb7ee3e1bd9e229b4
Srivarshan <srivarshan02@gmail.com>
2022-01-03 12:31:17 +0530

    Update Solution.java

--- Java/Java Regex 2 - Duplicate Words/Solution.java
+++ Java/Java Regex 2 - Duplicate Words/Solution.java
@@ -1,3 +1,6 @@
+// TODO: Complete solution
+
+/*
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -6,26 +9,27 @@ public class DuplicateWords {
 
     public static void main(String[] args) {
 
-	String regex = "/* Write a RegEx matching repeated words here. */";
-	Pattern p = Pattern.compile(regex, /* Insert the correct Pattern flag here.*/);
+        // String regex = " Write a RegEx matching repeated words here. ";
+        // Pattern p = Pattern.compile(regex,  Insert the correct Pattern flag here.);
 
-	Scanner scanner = new Scanner(System.in);
-	int numSentences = Integer.parseInt(scanner.nextLine());
-
-	while (numSentences-- > 0) {
-	    String input = scanner.nextLine();
-
-	    Matcher m = p.matcher(input);
-
-	    // Check for subsequences of input that match the compiled pattern
-	    while (m.find()) {
-		input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
-	    }
-
-	    // Prints the modified sentence.
-	    System.out.println(input);
-	}
-
-	scanner.close();
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                // input = input.replaceAll( The regex to replace ,  The replacement. );
+            }
+            
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        
+        in.close();
     }
 }
+*/
