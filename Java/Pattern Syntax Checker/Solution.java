2325c8d516e6ed9261127ffe4de640eeb2bce02c
Srivarshan <srivarshan02@gmail.com>
2022-01-03 10:30:07 +0530

    Update Solution.java

--- Java/Pattern Syntax Checker/Solution.java
+++ Java/Pattern Syntax Checker/Solution.java
@@ -3,12 +3,18 @@ import java.util.regex.*;
 
 public class Solution
 {
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
-		int testCases = Integer.parseInt(in.nextLine());
-		while(testCases>0){
-			String pattern = in.nextLine();
-			//Write your code
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testCases = Integer.parseInt(scanner.nextLine());
+		while (testCases > 0) {
+			String pattern = scanner.nextLine();
+			try {
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			} catch (Exception e) {
+				System.out.println("Invalid");
+			}
+			testCases--;
 		}
 	}
 }
