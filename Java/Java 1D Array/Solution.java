e5a445cc23d53c85277bf729407672072923486d
Srivarshan <srivarshan02@gmail.com>
2022-01-04 21:57:50 +0530

    Update Solution.java

--- Java/Java 1D Array/Solution.java
+++ Java/Java 1D Array/Solution.java
@@ -7,6 +7,11 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
+       int[] a = new int[n]; 
+       for (int i = 0; i < a.length; ++i) {
+           a[i] = scan.nextInt();
+       }
+
         scan.close();
 
         // Prints each sequential element in array a
