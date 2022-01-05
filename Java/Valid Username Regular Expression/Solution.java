4b42654904c0da0ead31814184612fd191272d81
Srivarshan <srivarshan02@gmail.com>
2022-01-05 08:05:20 +0530

    Update Solution.java

--- Java/Valid Username Regular Expression/Solution.java
+++ Java/Valid Username Regular Expression/Solution.java
@@ -3,7 +3,7 @@ class UsernameValidator {
     /*
      * Write regular expression here.
      */
-    public static final String regularExpression = null;
+    public static final String regularExpression = "^[A-Za-z]\\w{7,29}$";
 }
 
 
