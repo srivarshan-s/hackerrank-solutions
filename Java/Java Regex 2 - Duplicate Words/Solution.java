43067520369fc673656b84169551a450d820849c
Srivarshan <srivarshan02@gmail.com>
2022-01-06 10:40:07 +0530

    Update Solution.java

--- Java/Java Regex 2 - Duplicate Words/Solution.java
+++ Java/Java Regex 2 - Duplicate Words/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        String regex = "/* Write a RegEx matching repeated words here. */";
+        String regex = "\\b(\\w+)(?:\\W+\\1\\b)+";
         Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
         Scanner in = new Scanner(System.in);
@@ -19,7 +19,8 @@ public class Solution {
             
             // Check for subsequences of input that match the compiled pattern
             while (m.find()) {
-                input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
+                // input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
+                input = input.replaceAll(regex, "");
             }
             
             // Prints the modified sentence.
